e0c252ad920681a964919cc8b88827df975348d2
arbanhossain <arbanhossain@gmail.com>
2022-02-22 22:25:29 +0600

    fetch and display basic info

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,92 @@
   <div class="visualization">
     <MapView :map_name="map_name" :plot_src="plot_src" />
   </div>
-    <div class="options_menu">
-      <Options :player_list="player_list" />
-    </div>
+  <div class="options_menu">
+    <Options
+      @add-match-id="add_new_match"
+      :player_list="player_list"
+      :match_info="match_info"
+    />
+  </div>
 </template>
 
 <script>
-import Options from '@/components/Options.vue'
-import MapView from '@/components/MapView.vue'
+import Options from "@/components/Options.vue";
+import MapView from "@/components/MapView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Options,
-    MapView
+    MapView,
   },
   data() {
     return {
-      player_list: ['liquidicalpants', 'Leminscate'],
-      match_id_collection: [],
+      player_list: [],
+      match_id_collection: {},
       kill_data: [],
-      map_name: 'no_map',
+      matches: [],
+      match_info: [
+
+      ],
+      map_name: "no_map",
       plot_src: "",
-    }
-  }
-}
+    };
+  },
+  methods: {
+    async add_new_match(id) {
+      if (this.match_id_collection[id] == undefined) {
+        try {
+          let response = await fetch(
+            "https://valorant-heatmap.herokuapp.com/api/matches/" + id,
+            {
+              method: "GET",
+            }
+          );
+
+          let string = await response.text();
+          let json = string === "" ? {} : JSON.parse(string);
+
+          console.log(json);
+
+          let new_match_id_collection = this.match_id_collection;
+          new_match_id_collection[id] = json;
+          this.match_id_collection = new_match_id_collection;
+
+          let new_match_info = this.match_info;
+          new_match_info.push({
+            value: id,
+            label: json["map_details"].name,
+            help: id
+          });
+          this.match_info = new_match_info;
+
+          let players = json["players"];
+
+          
+          let new_player_list = this.player_list;
+          for (const key in players) {
+            let player = players[key];
+            //console.log(player)
+            if(new_player_list.some( elem => elem.value === player.id )){
+              console.log(player)
+            } else {
+              new_player_list.push({
+                value: player.id,
+                label: player.name,
+                help: player.id
+              });
+            }
+          }
+          this.player_list = new_player_list;
+          console.log(new_player_list)
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+  },
+};
 </script>
 
 <style>
--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,6 +1,15 @@
 <template>
+  <FormKit type=button label="Add Match" @click="handleAddMatch" />
   <FormKit type="form" :actions="false" v-model="opt" @submit="handleSubmit">
     <FormKit
+      v-if="match_info.length > 0"
+      type="checkbox"
+      label="Matches"
+      :options="match_info"
+      v-model="opt.matches"
+    />
+    <FormKit
+    v-if="player_list.length > 0"
       type="checkbox"
       label="Players"
       :options="player_list"
@@ -20,7 +29,12 @@
       max="200"
     />
     <pre wrap>{{ opt.time }}</pre>
-    <FormKit type="text" label="Round No" v-model="opt.round_num" help="Space separated round numbers" />
+    <FormKit
+      type="text"
+      label="Round No"
+      v-model="opt.round_num"
+      help="Space separated round numbers"
+    />
     <FormKit type="submit" label="Submit" />
   </FormKit>
 </template>
@@ -28,7 +42,8 @@
 <script>
 export default {
   name: "Options",
-  props: ["player_list"],
+  props: ["player_list", "match_info"],
+  emits: ["add-match-id"],
   data() {
     return {
       opt: {
@@ -36,23 +51,45 @@ export default {
         side: [],
         time: 0,
         roundNum: "",
+        matches:[],
       },
     };
   },
   methods: {
     handleSubmit(e) {
-      let rounds = []
-      if(e.round_num !== "") {
+      let rounds = [];
+      if (e.round_num !== "") {
         let r = e.round_num.split(" ");
-        r.forEach(element => {
-          rounds.push(parseInt(element) - 1)
+        r.forEach((element) => {
+          rounds.push(parseInt(element) - 1);
         });
       }
       //console.log(this.opt);
-      let options_object = { "player": e.player, "side": e.side, "time": e.time, "round": rounds };
+      let options_object = {
+        player: e.player,
+        side: e.side,
+        time: e.time,
+        round: rounds,
+      };
 
       console.log(options_object);
+    },
+    handleAddMatch(){
+      let prompt = window.prompt("Enter match id");
+      this.$emit('add-match-id', prompt);
     },
   },
 };
 </script>
+
+<style>
+.blue-btn {
+  background-color: #0275ff;
+  color: aliceblue;
+  cursor: pointer;
+  transition-property: filter;
+    transition-duration: 0.25s;
+    transition-timing-function: ease;
+    transition-delay: 0s;
+}
+</style>
